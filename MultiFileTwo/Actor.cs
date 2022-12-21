2999b89e3496a5bf64b2b8c74b56a0d374544cbf
Paul Lesieur <plesieur@jpkeefehs.org>
2022-12-21 13:53:52 -0500

    Add project files.

---
+++ MultiFileTwo/Actor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MultiFileTwo
+{
+    internal class Actor
+    {
+        private string _name;
+        private string _movie;
+
+        public Actor(string Name, string Movie)
+        {
+            _name = Name;
+            _movie = Movie;
+        }
+
+        public string Name { 
+            get { return _name; } 
+            set { _name = value; }
+        }
+
+        public string Movie {
+            get { return _movie; }
+            set { _movie = value; }
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("{0} appeared in the movie '{1}.'",this._name,this._movie);
+        }
+    }
+}
---
+++ MultiFileTwo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MultiFileTwo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Actor> actors = new List<Actor> {
+                new Actor("Gene Hackman", "The Poseidon Adventure"),
+                new Actor("Ally Sheedy", "War Games")
+            }; 
+            //Add your Object for your new Classes here
+
+            Console.WriteLine("Hello Class!\n");
+
+            actors.Add(new Actor("Richard Dreyfuss", "Close Encounters of the Third Kind" ));
+            for (int i = 0; i < actors.Count; i++) {
+                actors[i].Display();
+            }
+            Console.WriteLine("---------------------------");
+
+            //Add your additional object to your list here as well as the printing of your objects
+        }
+    }
+}
